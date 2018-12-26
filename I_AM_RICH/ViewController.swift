d26e001ef2f2db311f40b0b85b7b6d25f7bd674f
Sanskar <sansyrox@gmail.com>
2018-12-27 01:16:41 +0530

    Added the ios contents

---
+++ I_AM_RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I_AM_RICH
+//
+//  Created by Sanskar Jethi on 26/12/18.
+//  Copyright © 2018 Sanskar Jethi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
